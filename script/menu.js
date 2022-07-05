c3984dde9b2fea55458b6b9f6671112ddc20770c
hyeri0319 <108320604+hyeri0319@users.noreply.github.com>
2022-07-05 12:37:08 +0900

    Delete script directory

--- script/menu.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $('.menu1>li').mouseenter(function(){
-        $(this).children('.sub').stop().slideDown();
-    });
-
-    $('.menu1>li').mouseleave(function(){
-        $(this).children('.sub').stop().slideUp();
-    });
-});
